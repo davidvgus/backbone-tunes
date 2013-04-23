976a5d35d4b1e39cd8d416c58228ba29634801c1
David Gustafson <git@radicalskeptic.net>
2013-04-22 17:31:09 -0700

    added second and third tests

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -1,3 +1,32 @@
 (function($) {
+  window.Album = Backbone.Model.extend({
+
+    isFirstTrack: function(index){
+      return index == 0;
+    },
+
+    isLastTrack: function(index){
+      return index >= this.get('tracks').length -1;
+    }
+  });
+
+  window.AlbumView = Backbone.View.extend({
+    tagName: 'li',
+    className: 'album',
+
+    initialize: function() {
+      //_.bindAll(this, 'render');
+      //this.model.bind('change', this.render);
+      this.model.on('change', this.render, this);
+
+      this.template = _.template($('#album-template').html());
+    },
+
+    render: function() {
+      var renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent);
+      return this;
+    }
+  });
 
 })(jQuery);
